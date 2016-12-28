a380de1cef428c45b81981ccc3925738ae14aa4c
Erik Jönsson <efjonsson@gmail.com>
2016-12-28 16:04:20 -0500

    build basic filter list, toggle expanded filter groups on click and selected filter items on click

--- js/landing-page.js
+++ js/landing-page.js
@@ -1,1 +1,25 @@
-console.log('landing page');
+function expandFilterList() {
+  const filterGroupHeaders = document.querySelectorAll('.filter-group__name');
+  filterGroupHeaders.forEach(function (filterGroupHeader) {
+    filterGroupHeader.addEventListener('click', function(e) {
+      const filterGroup = e.target.parentElement;
+      filterGroup.classList.toggle('filter-group--expanded');
+    });
+  });
+}
+
+function selectFilter() {
+  const filterItems = document.querySelectorAll('.filter-group__item');
+  filterItems.forEach(function(filterItem) {
+    filterItem.addEventListener('click', function(e) {
+      e.target.classList.toggle('filter-group__item--selected');
+    });
+  });
+}
+
+function addClickListeners() {
+  expandFilterList();
+  selectFilter();
+}
+
+window.onload = addClickListeners;
